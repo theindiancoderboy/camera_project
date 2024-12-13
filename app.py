43aff420463a83c5f4cb2b7cf8dafeaeedbe6337
Ajoy Mondal <ajoy.mondal@omnimindsconsulting.com>
2024-12-13 21:29:29 +0530

    added'

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ pipeline = dai.Pipeline()
 
 # Define sources and outputs
 camRgb = pipeline.create(dai.node.ColorCamera)
-camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4K)
 camRgb.setPreviewSize(1080,1080)
 camRgb.setInterleaved(False)
 camRgb.initialControl.setManualFocus(145)
